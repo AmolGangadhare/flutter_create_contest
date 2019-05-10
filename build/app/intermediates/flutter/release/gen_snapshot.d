338dce94c8191fa2e92a666ef8b240092d982667
Amol Gangadhare <amol.gangadhare@centelon.com>
2019-05-10 16:38:28 +0530

    Cleaned unused code

--- build/app/intermediates/flutter/release/gen_snapshot.d
+++
@@ -1,1 +0,0 @@
-gen_snapshot.d: /Volumes/MyData/Flutter_SDK/flutter/bin/cache/artifacts/engine/android-arm-release/darwin-x64/gen_snapshot
--- build/frontend_server.d
+++
@@ -1,1 +0,0 @@
-frontend_server.d: /Volumes/MyData/Flutter_SDK/flutter/bin/cache/artifacts/engine/darwin-x64/frontend_server.dart.snapshot
